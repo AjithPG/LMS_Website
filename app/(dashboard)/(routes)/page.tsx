d568c213de7e2964ffe5468188cd7ca3a6595c0c
Ajith PG <ajithpg008@gmail.com>
2023-10-27 23:17:01 +0530

    Initail Upload

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-medium text-sky-700">
+        This is protected page
+      </h1>
+    </div>
+  );
+}
